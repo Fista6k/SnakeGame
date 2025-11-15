604f13fd479c2a3450a5c9bd763d5f36a4f9b9fc
enjxyer <hfgtylop@gmail.com>
2025-11-15 14:42:05 +0500

    add library ebiten

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+type Game struct {
+}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return 320, 240
+}
+
+func main() {
+	game := &Game{}
+	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowTitle("Snake")
+	if err := ebiten.RunGame(game); err != nil {
+		log.Fatal(err)
+	}
+}
